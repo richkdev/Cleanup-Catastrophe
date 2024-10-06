b0dc6eb655b6332567fcce4ac384fabbc886889d
richmondkleinson <richardkurniawan621@gmail.com>
2024-10-06 16:48:07 +0700

    fixed zengl_test cross-compatibility

--- tests/zengl_test/main.py
+++ tests/zengl_test/main.py
@@ -8,29 +8,29 @@
 # based on:
 #   https://github.com/szabolcsdombi/zengl/blob/main/examples/pygbag/main.py
 #   https://github.com/pygame-web/showcases/blob/main/org/zengl/zengl-normal-mapping/zengl-normal-mapping.py
+
 # run this command
-# pygbag --PYBUILD 3.12 --ume_block 0 --template noctx.tmpl "tests\zengl_test\main.py"
+#   pygbag --PYBUILD 3.12 --ume_block 0 --template noctx.tmpl "tests\zengl_test\main.py"
 
 import asyncio
-import os
-import sys
 import pygame
 import zengl
 
 pygame.init()
 pygame.display.set_mode((640, 480), flags=pygame.OPENGL, vsync=True)
 
-# pygame.display.gl_set_attribute(pygame.GL_CONTEXT_MAJOR_VERSION, 3)
-# pygame.display.gl_set_attribute(pygame.GL_CONTEXT_MINOR_VERSION, 0)
+pygame.display.gl_set_attribute(pygame.GL_CONTEXT_MAJOR_VERSION, 3)
+pygame.display.gl_set_attribute(pygame.GL_CONTEXT_MINOR_VERSION, 0)
 
 ctx = zengl.context()
 
 window_size = pygame.display.get_window_size()
-image = ctx.image(window_size, 'rgba8unorm')
 overlay = ctx.image(window_size, 'rgba8unorm')
 
 vertexShader = """
 #version 300 es
+
+precision highp float;
 
 vec2 vertices[3] = vec2[](
     vec2(-1.0, -1.0),
@@ -49,8 +49,9 @@ void main() {
 fragmentShader = """
 #version 300 es
 
-uniform sampler2D Layer1;
-uniform sampler2D Layer2;
+precision highp float;
+
+uniform sampler2D screen_texture;
 
 in lowp vec2 vertex;
 
@@ -58,9 +59,8 @@ layout (location = 0) out vec4 out_color;
 
 void main() {
     vec2 uv = vertex * 0.5 + 0.5;
-    vec4 color1 = texture(Layer1, uv);
-    vec4 color2 = texture(Layer2, uv);
-    out_color = color1 * (1.0 - color2.a) + color2 * color2.a;
+    vec4 color2 = texture(screen_texture, uv);
+    out_color = color2 * color2.a;
 }
 """
 
@@ -69,23 +69,14 @@ pipeline = ctx.pipeline(
     fragment_shader=fragmentShader,
     layout=[
         {
-            'name': 'Layer1',
+            'name': 'screen_texture',
             'binding': 0,
-        },
-        {
-            'name': 'Layer2',
-            'binding': 1,
         },
     ],
     resources=[
         {
             'type': 'sampler',
             'binding': 0,
-            'image': image,
-        },
-        {
-            'type': 'sampler',
-            'binding': 1,
             'image': overlay,
         },
     ],
@@ -108,15 +99,12 @@ async def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-                sys.exit()
 
         # mouse_x, mouse_y = pygame.mouse.get_pos()
-        # mouse_x = mouse_x / size[0] - 0.5
-        # mouse_y = 0.5 - mouse_y / size[1]
+        # mouse_x = mouse_x / window_size[0] - 0.5
+        # mouse_y = 0.5 - mouse_y / window_size[1]
 
         ctx.new_frame()
-        image.write(os.urandom(window_size[0] * window_size[1] * 4))
-
         screen.fill((0, 0, 0))
         screen.blit(rectangle, (10, 10))
 
