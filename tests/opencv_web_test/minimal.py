12cbe61c7751fa8cc8ba6aa4b6831dca5e80fed5
richmondkleinson <richardkurniawan621@gmail.com>
2025-04-12 16:13:12 +0700

    incomplete test, committing for review

--- tests/opencv_web_test/minimal.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import os
-
-print(cv2.getBuildInformation())
-print("ffmpeg accessable?", os.system("cd /; ffmpeg.exe"))
-
-vid = cv2.VideoCapture()
-print("VideoCapture opened successfully?", vid.open("http://127.0.0.1:5500/tests/opencv_web_test/video.mp4"))
